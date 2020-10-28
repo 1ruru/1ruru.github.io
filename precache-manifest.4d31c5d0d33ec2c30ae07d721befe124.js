6bc7984045fe80534c0ac438d086c5538a1f0ece
SekSakai <seiji.sakai@sekishin.co.jp>
2020-10-28 11:30:19 +0900

    add

---
+++ precache-manifest.4d31c5d0d33ec2c30ae07d721befe124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6c68882bbc90e7f62c",
+    "url": "css/chunk-vendors.99aead83.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "504cf33ef01fa9358ab20f594dad742f",
+    "url": "index.html"
+  },
+  {
+    "revision": "ecc71b92284bd2603aec",
+    "url": "js/app.7608bf9e.js"
+  },
+  {
+    "revision": "6b6c68882bbc90e7f62c",
+    "url": "js/chunk-vendors.ade974df.js"
+  },
+  {
+    "revision": "6231da3b1721e05e630f53c1a04a5e23",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,34 @@
-// service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.4d31c5d0d33ec2c30ae07d721befe124.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "spa-pr4"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-});
-
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
